6aba0e18f74ac78e040d132e311472bf5d74de6c
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2024-12-28 02:21:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function invbut(){
+  location.replace('https://discord.com/oauth2/authorize?client_id=1062733677513158687&scope=bot%20applications.commands&permissions=2146958847');
+}
+function invbut2(){
+  location.replace('https://discord.com/oauth2/authorize?client_id=1062733677513158687&scope=bot%20applications.commands&permissions=2146958847')
+}
