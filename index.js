7e4eca7fb093e63a49b67fb33520fff09173efa6
Raymondoes <130866935+Raymondoes@users.noreply.github.com>
2025-01-02 15:31:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,5 +2,27 @@ function invbut(){
   location.replace('https://discord.com/oauth2/authorize?client_id=1062733677513158687&scope=bot%20applications.commands&permissions=2146958847');
 }
 function invbut2(){
-  location.replace('https://discord.com/oauth2/authorize?client_id=1062733677513158687&scope=bot%20applications.commands&permissions=2146958847')
+  location.replace('https://discord.com/oauth2/authorize?client_id=1062733677513158687&scope=bot%20applications.commands&permissions=2146958847');
 }
+function guidelines(){
+  location.replace('guidelines.html');
+} 
+
+document.addEventListener("DOMContentLoaded", () => {
+  const menuItems = document.querySelectorAll(".menu-item");
+
+  menuItems.forEach((item) => {
+    const header = item.querySelector(".menu-header");
+    const content = item.querySelector(".menu-content");
+    const icon = item.querySelector(".menu-icon");
+
+    header.addEventListener("click", () => {
+      const isHidden = content.classList.contains("hidden");
+
+      // Toggle visibility
+      content.classList.toggle("hidden", !isHidden);
+      icon.classList.toggle("rotate-180", isHidden);
+    });
+  });
+});
+
